14fb03e50ff1f5a2d14cd05308a62a8791f576ca
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2024-06-20 11:40:40 +0500

    ddd

--- app.py
+++ app.py
@@ -117,6 +117,5 @@ def key_events_get():
     return jsonify({'keys': events_to_execute})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('cert.pem', 'key.pem'))
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run()
